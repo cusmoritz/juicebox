134ba86de002a46e24fd414aeed46bf8005c9892
cusmoritz <cus.moritz@gmail.com>
2022-11-17 13:22:31 -0700

    added auth to apiRouter

--- api/index.js
+++ api/index.js
@@ -1,5 +1,39 @@
+require('dotenv').config();
+
 const express = require('express');
 const apiRouter = express.Router();
+
+// this stuff will allow us to create tokens
+const jwt = require('jsonwebtoken');
+const { getUserById } = require('../db');
+const { JWT_SECRET } = process.env;
+
+apiRouter.use(async (request, response, next) => {
+    const prefix = 'Bearer ';
+    const auth = request.header('Authorization');
+
+    if (!auth) {
+        next();
+    } else if (auth.startsWith(prefix)) {
+        const token = auth.slice(prefix.length);
+
+        try {
+            const { id } = jwt.verify(token, JWT_SECRET);
+
+            if (id) {
+                request.user = await getUserById(id);
+                next();
+            }
+        } catch ({ name, message}) {
+            next({ name, message });
+        }
+    } else {
+        next({
+            name: 'AuthorizationHeaderError',
+            message: `Authorization token must start with ${ prefix }`
+        });
+    }
+});
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
@@ -8,6 +42,6 @@ const postsRouter = require('./posts');
 apiRouter.use('/posts', postsRouter);
 
 const tagsRouter = require('./tags');
-apiRouter.use('/tags', tagsRouter);
+apiRouter.use('/tags', tagsRouter)
 
 module.exports = apiRouter;
