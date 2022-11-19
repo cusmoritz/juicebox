f9b5308d8a8e7f8b69fba99edee66da7ceccc485
cusmoritz <cus.moritz@gmail.com>
2022-11-19 11:10:48 -0700

    added tagsRouter/GET to search for posts by tags

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const tagsRouter = express.Router();
 
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.get('/', async(request, response) => {
     const tags = await getAllTags();
@@ -11,5 +11,24 @@ tagsRouter.get('/', async(request, response) => {
     });
 
 });
+
+tagsRouter.get('/:tagName/posts', async(request, respond, next) => {
+    // grab the tagname from the url params
+    const { tagName } = request.params;
+    
+    try {
+        const posts = await getPostsByTagName(tagName);
+        console.log('posts in tagsRouter: ', posts);
+        respond.send({posts: posts});
+    } catch (error) {
+        console.log('there was an error in tagsRouter/GET: ', error);
+        next({
+            name: "TagsGETError",
+            message: "There was an error getting posts with tags."
+        });
+    }
+});
+
+
 
 module.exports = tagsRouter;
