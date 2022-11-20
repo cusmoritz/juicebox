ce9cfdb77ded850c4842a9c812071670a39358f6
cusmoritz <cus.moritz@gmail.com>
2022-11-20 15:02:31 -0700

    changes being made 2

--- db/index.js
+++ db/index.js
@@ -5,12 +5,10 @@ const { DATABASE_URL = 'postgres://localhost:5432/juicebox-dev' } = process.env;
 
 // const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev';
 
-const client = new Client();
-
-// {
-//     connectionString: DATABASE_URL,
-//     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-// }
+const client = new Client({
+    connectionString: DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 const getAllUsers = async() => {
 
