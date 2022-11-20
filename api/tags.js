89d4ebdf0fb6ca74b35d053647b89fe4aeb6428d
cusmoritz <cus.moritz@gmail.com>
2022-11-20 13:54:10 -0700

    added tagsRouter.GET to only return active posts

--- api/tags.js
+++ api/tags.js
@@ -15,14 +15,19 @@ const { getAllTags, getPostsByTagName } = require('../db');
 tagsRouter.get('/:tagName/posts', async(request, respond, next) => {
     // grab the tagname from the url params
     const { tagName } = request.params;
+    // console.log('this is the request: ', request.params.tagName)
     // const encodedTag = encodeURIComponent(tagName);
     console.log('tag name in tagsrouter/GET ', tagName);
 
     try {
-        const posts = await getPostsByTagName(tagName);
-        console.log('posts in tagsRouter: ', posts);
+        // get all the posts with that tag
+        const allTagPosts = await getPostsByTagName(tagName);
         
-        respond.send({posts: posts});
+        // filter them to make sure they are active
+        const activePosts = allTagPosts.filter( eachPost => eachPost.active === true);
+        
+        // return active posts
+        respond.send({posts: activePosts});
         
     } catch (error) {
         console.log('there was an error in tagsRouter/GET: ', error);
